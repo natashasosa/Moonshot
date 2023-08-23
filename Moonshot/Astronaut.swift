7e986322f4188541da81feef8dd685de028dc9e3
natashasosa <natasha.sosa@poatek.com>
2023-08-23 13:50:49 -0300

    first commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Natasha Rebelo on 16/08/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
